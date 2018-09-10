3bb7827fabb7270ce20ad1435f09a012554ddc62
Frank Mueller <frank.mueller@linking-you.de>
2018-09-10 21:19:10 +0200

    Fixed contao 4.4 compatibility

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of ErdmannFreunde/contao-news-meta
+ *
+ * (c) 2018 Erdmann & Freunde.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package    euf_hero
+ * @author     Dennis Erdmann
+ * @author     Frank Müller
+ * @copyright  2018 Erdmann & Freunde
+ * @license    LICENSE LGPL-3.0
+ *
+ */
+
+$GLOBALS["TL_HOOKS"]['generatePage'][] = array(
+    'EuF\\ContaoNewsMeta\\Classes\\NewsMeta',
+    'onGeneratePage'
+);
